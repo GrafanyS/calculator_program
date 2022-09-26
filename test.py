d5e1c33a8b48607cf64deaa093d890b4308db781
GrafanyS <admin@valet.of.by>
2022-09-26 22:54:59 +0300

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+﻿import ui
+
+
+def get_ints():
+    '''
+    проверка на ввод числа
+    '''
+    while True:
+        try:
+            num = int(input('value = '))
+            return num
+        except ValueError:
+            print('Ошибка. Ожидалось вещественное число.')
