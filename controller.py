78ebe3d9033df0821177d51af6fbe7a8eafc630c
GrafanyS <admin@valet.of.by>
2022-09-26 22:51:45 +0300

    controller added

---
+++ controller.py
@@ -0,0 +1,30 @@
+import complex
+import rational
+# import loger
+import main
+# import real
+
+
+def button_clic(complex_operation):
+    user_komplex1 = complex.get_complex()
+    user_komplex2 = complex.get_complex()
+    operation = complex.get_operation()
+    # complex_a = complex.get_complex()
+    # complex_b = complex.get_complex()
+    # result = complex_operation(result, complex_a, complex_b)
+    # complex_a = result
+    # complex_b = result
+
+
+def button_clic(get_rational):
+    list_dig = rational.get_rational()
+    list_dig = rational.get_rational()
+    list_operator = rational.get_rational()
+    list_one_num = rational.get_rational()
+    znak = 1
+
+    # complex_a = complex.get_complex()
+    # complex_b = complex.get_complex()
+    # result = complex_operation(result, complex_a, complex_b)
+    # complex_a = result
+    # complex_b = result
