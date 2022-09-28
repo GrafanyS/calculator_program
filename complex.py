8181c0fa4d68bd04d2735cb8887d0d56cd06ec8a
GrafanyS <admin@valet.of.by>
2022-09-28 20:28:58 +0300

    .\complex.py

--- complex.py
+++ complex.py
@@ -1,27 +1,28 @@
 import cmath
 
+# сложение значений
 def add(a, b):
     return a + b
 
-
+# умножение значений
 def mult(a, b):
     return a * b
 
-
+# вычитание значений
 def diff(a, b):
     return a - b
 
-
+ # деление значений
 def div(a, b):
     if not b:
         return 'Деление на ноль'
     return a / b
 
-
+# возведение в степерь значений
 def power(a, n):
     return a ** n
 
-
+# извлечение корня в степени (a)
 def sqrt(a):
     b = cmath.sqrt(a)
     if not b.imag:
