393045314eccb383f8e95dba08566bb55cee7fd8
GrafanyS <admin@valet.of.by>
2022-09-28 11:27:29 +0300

    logger все обвернул в функции

--- logger.py
+++ logger.py
@@ -2,30 +2,46 @@ from datetime import datetime as dt
 import logging
 from time import time
 
-logging.basicConfig(level=logging.DEBUG, format= '%(asctime)s - %(levelname)s - %(message)s' )
-logging.debug( 'Это сообщение журнала.' )
 
-
-logging.basicConfig(level=logging.INFO, filename="result.txt",filemode="a")
-logging.debug("A DEBUG Message")
-logging.info("An INFO")
-logging.warning("A WARNING")
-logging.error("An ERROR")
-logging.critical("A message of CRITICAL severity")
-
-def data_recording(value_x, value_y, value_z, rezult):
+def data_recording(result):
     '''
-    метод принимает значения x, y, z и результат вычисления
-    и записывает в файл .txt с указанием времени
+    метод принимает значения и результат вычисления
+    и записывает в файл .log с указанием времени
     '''
 
     time = dt.now().strftime('%d-%b-%y %H:%M:%S')
-    with open('result.txt', 'a', encoding='utf-8') as file:
+    with open('logger.log', 'a', encoding='utf-8') as file:
         file.write(time + '\n')
-        file.write('\n' + f'{value_x} = value_x' + '\n')
-        file.write(f'{value_y} = value_y' + '\n')
-        file.write(f'"{value_z}" = value_z' + '\n')
-        file.write(f'результат вычисления = {rezult}')
+        file.write(f'результат вычисления = {result}')
 
+# data_recording()
 
+def log(data, result):
+    '''функция log  принимает значения и результат вычисления
+    и записывает в файл .log с указанием времени'''
+    time = dt.now().strftime('%d-%b-%y %H:%M:%S')
+    with open('logger.log', 'a') as file:
+        file.writelines(f'{time} : {data}  {result}\n')
+    
+def logger(data, result):
+    '''функция logger использование сандартной библиотеки logging '''
+    logger2 = logging.getLogger(__name__)
 
+    logger2.setLevel(logging.INFO)
+
+    # настройка обработчика и форматировщика для logger2
+    handler2 = logging.FileHandler(f"{__name__}.log", mode='a')
+    formatter2 = logging.Formatter(
+        "%(name)s %(asctime)s %(levelname)s %(message)s")
+
+    # добавление форматировщика к обработчику
+    handler2.setFormatter(formatter2)
+    # добавление обработчика к логгеру
+    logger2.addHandler(handler2)
+
+    logger2.debug('Это сообщение журнала.')
+    logger2.info(
+        f"Тестирование пользовательского регистратора для модуля {__name__}...")
+    logger2.warning(f"ПРЕДУПРЕЖДЕНИЕ {__name__}...")
+    logger2.error("ОШИБКА")
+    logger2.critical("Сообщение КРИТИЧЕСКОЙ серьезности\n")
