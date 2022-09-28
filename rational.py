e8b423a13247961a1f301144fa534e13ee961e53
GrafanyS <admin@valet.of.by>
2022-09-28 20:29:31 +0300

    .\rational.py

--- rational.py
+++ rational.py
@@ -4,7 +4,6 @@ import math
 x = 0
 y = 0
 
-
 # фунцкия для инициализации значении
 def init(a, b):
     global x
@@ -12,21 +11,22 @@ def init(a, b):
     x = a
     y = b
     
-
 # функция для инициализации одного значения для операций типа sqrt
-
-
 def init_value(a):
     global x
     x = a
 
-
-def add(): return x + y  # сложение значений
-def diff(): return x - y  # вычитание значений
-def mult(): return x * y  # умножение значений
-def div():                 # деление значений
+# сложение значений
+def add(): return x + y  
+# вычитание значений
+def diff(): return x - y  
+# умножение значений
+def mult(): return x * y  
+# деление значений
+def div():                 
     try:
         return x / y
     except ZeroDivisionError:
         print("Деление на ноль") 
-def power(): return x ** y  # возведение в степерь значений
+ # возведение в степерь значений
+def power(): return x ** y 
